b360536e7c99a18b36458def2538feb3822413f9
Ahmed Abou Raya <a.abouraya@gmail.com>
2019-09-18 11:23:11 +0200

    enable Hystrix

--- composite-product/src/main/java/com/knowhow/poc/composite/CompositeProductApplication.java
+++ composite-product/src/main/java/com/knowhow/poc/composite/CompositeProductApplication.java
@@ -2,10 +2,14 @@ package com.knowhow.poc.composite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CompositeProductApplication {
 
 	public static void main(String[] args) {
--- composite-product/src/main/java/com/knowhow/poc/composite/service/impl/ProductServiceImpl.java
+++ composite-product/src/main/java/com/knowhow/poc/composite/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.knowhow.poc.composite.service.impl;
 
 import com.knowhow.poc.composite.service.ProductService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,13 @@ public class ProductServiceImpl implements ProductService {
 
 
     @Override
+    @HystrixCommand(fallbackMethod = "defaultFindCore")
     public String findCore(String id) {
-        return rest.getForObject("http://CORE-PRODUCT/core-service/{id}", String.class, id);
+        return rest.getForObject("http://core-product/core-service/{id}", String.class, id);
     }
 
-
+    public String defaultFindCore(String id) {
+        return "this is the default return for id: " + id;
+    }
 
 }
